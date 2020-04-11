ad829d97c3dee74206cc9e06f6a3e417479e51bc
bmpschneider <brunopschneider@gmail.com>
2020-04-10 22:58:22 -0300

    em andamento

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutterappcontactlist/ui/contact_page.dart';
+import 'package:flutterappcontactlist/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
